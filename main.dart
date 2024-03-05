6d41be9f4fea19d65d348396a761a19d3c5f3926
CesarMure <118925352+CesarMure@users.noreply.github.com>
2024-03-05 21:29:32 +0000

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,36 +1,13 @@
 import 'package:flutter/material.dart';
-
+//lesson 1
 void main() {
   return runApp(MaterialApp(
     home: Scaffold( 
-      appBar: AppBar(title: Text("Hello World"),
-      backgroundColor: Colors.red[300],
+      appBar: AppBar(
+        title: Text("Hello World"),
+        backgroundColor: Colors.deepOrange[400],
       ),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-          color: Colors.red[200],
-          
-        ),
-      child: Text("Child one"),
-        ),
-        Container(
-          decoration: BoxDecoration(
-          color: Colors.red[400],
-        ),
-      child: Text("Child two"),
-        ),
-        Container(
-          decoration: BoxDecoration(
-          color: Colors.red[600],
-        ),
-      child: Text("Child three"),
-        ),
-      ]
     ),
-  )));
+  ));
 }
 
